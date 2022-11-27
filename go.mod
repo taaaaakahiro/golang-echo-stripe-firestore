fa0dad896b2047e857ddf484d2b76237e31952ae
takahiro <takapi854@gmail.com>
2022-11-27 22:48:45 +0900

    push

---
+++ go.mod
@@ -0,0 +1,5 @@
+module stripe-example
+
+go 1.19
+
+require github.com/stripe/stripe-go/v74 v74.1.0
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/price"
+	"github.com/stripe/stripe-go/v74/product"
+)
+
+func main() {
+	stripe.Key = os.Getenv("SECRET_KEY")
+
+	product_params := &stripe.ProductParams{
+		Name:        stripe.String("Starter Subscription"),
+		Description: stripe.String("$12/Month subscription"),
+	}
+	starter_product, _ := product.New(product_params)
+
+	price_params := &stripe.PriceParams{
+		Currency: stripe.String(string(stripe.CurrencyUSD)),
+		Product:  stripe.String(starter_product.ID),
+		Recurring: &stripe.PriceRecurringParams{
+			Interval: stripe.String(string(stripe.PriceRecurringIntervalMonth)),
+		},
+		UnitAmount: stripe.Int64(1200),
+	}
+	starter_price, _ := price.New(price_params)
+
+	fmt.Println("Success! Here is your starter subscription product id: " + starter_product.ID)
+	fmt.Println("Success! Here is your starter subscription price id: " + starter_price.ID)
+
+}
