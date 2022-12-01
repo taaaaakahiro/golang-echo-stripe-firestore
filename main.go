d61d49bcdef16bfbf90740729a699f22a565699b
takahiro <takapi854@gmail.com>
2022-12-01 22:02:07 +0900

    init firestore

--- main.go
+++ main.go
@@ -1,15 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+
+	"cloud.google.com/go/firestore"
 
 	"github.com/stripe/stripe-go/v74"
 	"github.com/stripe/stripe-go/v74/price"
 	"github.com/stripe/stripe-go/v74/product"
 )
 
+var (
+	fsClient *firestore.DocumentSnapshot
+)
+
 func main() {
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, "glassy-keyword-370311")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cli, err := client.Collection("golang-stripe").Doc("6gwybq6jqnSM3J0glk9V").Get(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cli.Data())
+	fsClient = cli
+	Init()
+
+}
+
+func Init() {
 	stripe.Key = os.Getenv("SECRET_KEY")
 
 	product_params := &stripe.ProductParams{
