3e07e97585570ce1f92429cfdaff8f2133412b51
takahiro <takapi854@gmail.com>
2022-12-11 21:58:25 +0900

    push

--- external/stripe/customer.go
+++ external/stripe/customer.go
@@ -8,8 +8,12 @@ import (
 	"github.com/stripe/stripe-go/customer"
 )
 
-func (s *Stripe) GetCustomer() {
-
+func (s *Stripe) GetCustomer(id string) (*stripe.Customer, error) {
+	c, err := customer.Get(id, nil)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return c, nil
 }
 
 func (s *Stripe) CreateCustomer(p *input.Customer) (*stripe.Customer, error) {
--- external/stripe/customer_test.go
+++ external/stripe/customer_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stripe/stripe-go"
 	"github.com/stripe/stripe-go/customer"
 )
 
@@ -22,6 +23,33 @@ func TestStripe_CreateCustomer(t *testing.T) {
 
 		t.Cleanup(func() {
 			_, err := customer.Del(got.ID, nil)
+			assert.NoError(t, err)
+		})
+	})
+}
+
+func TestStripe_GetCustomer(t *testing.T) {
+
+	t.Run("GetCustomer", func(t *testing.T) {
+		test := input.Customer{
+			Name: "user1",
+		}
+		params := &stripe.CustomerParams{
+			Name: &test.Name,
+		}
+		c, err := customer.New(params)
+		assert.NoError(t, err)
+		t.Run("ok", func(t *testing.T) {
+			got, err := testStripe.GetCustomer(c.ID)
+			assert.NoError(t, err)
+			assert.NotEmpty(t, got)
+
+			assert.Equal(t, "user1", got.Name)
+
+		})
+
+		t.Cleanup(func() {
+			_, err := customer.Del(c.ID, nil)
 			assert.NoError(t, err)
 		})
 	})
