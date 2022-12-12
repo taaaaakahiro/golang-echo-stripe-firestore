c22744c7aa75927ad71787181aa7045b847fcff4
takahiro <takapi854@gmail.com>
2022-12-13 00:19:09 +0900

    push

--- external/stripe/product.go
+++ external/stripe/product.go
@@ -1,1 +1,15 @@
 package stripe
+
+import (
+	"github.com/pkg/errors"
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/product"
+)
+
+func (s *Stripe) GetProduct(productID string) (*stripe.Product, error) {
+	p, err := product.Get(productID, nil)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return p, nil
+}
--- external/stripe/product_test.go
+++ external/stripe/product_test.go
@@ -1,1 +1,32 @@
 package stripe
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stripe/stripe-go/v74"
+	"github.com/stripe/stripe-go/v74/product"
+)
+
+func TestStripe_GetProduct(t *testing.T) {
+	stripe.Key = testStripe.Key
+	params := &stripe.ProductParams{
+		Name: stripe.String("product1"),
+	}
+	p, _ := product.New(params)
+
+	t.Run("ok", func(t *testing.T) {
+		got, err := testStripe.GetProduct(p.ID)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, got)
+
+		assert.Equal(t, "product1", got.Name)
+
+		t.Cleanup(func() {
+			_, err = product.Del(p.ID, nil)
+			assert.NoError(t, err)
+		})
+
+	})
+
+}
